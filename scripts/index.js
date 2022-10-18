9668078f4d518a1f4b9549abccb62baac59a9fd0
Leonardo Painenau <lamuraya2010@hotmail.com>
2022-10-18 20:33:19 -0300

    animacion boton

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+const navToggle = document.querySelector (".toggle")
+const navMenu = document.querySelector (".df-ul")
+navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle(".toggle_funcion")
+})
